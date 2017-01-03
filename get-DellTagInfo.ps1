be9ae0b409b6813663bd0a68446894e80ed5b5c2
DreamThief <git@dreamthief.net>
2017-01-02 19:12:43 -0600

    Remove kerfluffle

--- get-DellTagInfo.ps1
+++ get-DellTagInfo.ps1
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-﻿function get-dellTagInfo {  #lineBreak
-    param  ($svcTag) #lineBreak
-    $url = "https://api.dell.com/support/v2/assetinfo/warranty/tags.xml?svctags=" + $svcTag + "&apikey=849e027f476027a394edd656eaef4842" #lineBreak
-    Invoke-WebRequest -Uri $url | Select-Object -ExpandProperty content | Out-File .\"$svctag".dell.xml #lineBreak
-    [xml]$xml = Get-Content .\"$svctag".dell.xml #lineBreak
-    write-host "Service tag:" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.ServiceTag #lineBreak
-    write-host "ShipDate:" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.ShipDate #lineBreak
-    write-host "ModelInfo:" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.MachineDescription #lineBreak
-    write-host "Warranty End Date:" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.Warranties.Warranty[1].EndDate #lineBreak
-    write-host "Warranty Service Levels:"  #lineBreak
-    write-host "-----" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.Warranties.Warranty[0].ServiceLevelDescription #lineBreak
-    write-host "-----" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.Warranties.Warranty[1].ServiceLevelDescription #lineBreak
-    write-host "-----" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.Warranties.Warranty[2].ServiceLevelDescription #lineBreak
-    write-host "-----" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.Warranties.Warranty[3].ServiceLevelDescription #lineBreak
-    write-host "-----" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.Warranties.Warranty[4].ServiceLevelDescription #lineBreak
-    write-host "-----" $xml.GetAssetWarrantyResponse.GetAssetWarrantyResult.Response.DellAsset.Warranties.Warranty[5].ServiceLevelDescription #lineBreak
-} #lineBreak
-=======
-function get-dellTagInfo {  
+﻿function get-dellTagInfo {  
     param  ($svcTag) 
     $url = "https://api.dell.com/support/v2/assetinfo/warranty/tags.xml?svctags=" + $svcTag + "&apikey=849e027f476027a394edd656eaef4842" 
     try {
@@ -38,5 +19,4 @@ function get-dellTagInfo {
     Catch {
     Write-host "Could not find the service tag $svctag inside the Dell API"
     }     
-    }
->>>>>>> 7603aeb002bd894450e0c7b5ff77fb5e42b166c5
+    } 
