57a5325fadb4e1b5be4c5da2e90de440ba941721
DreamThief <git@dreamthief.net>
2017-02-25 09:12:12 -0600

    Generates a random phone number

---
+++ new-phonenumber.ps1
@@ -0,0 +1,12 @@
+﻿Function new-phoneNumber {    
+       param([int]$NumberToDo = 1)
+
+for ($genindex = 1; $genindex -le $NumberToDo; $genindex++) {
+
+    $area = get-random -Minimum 200 -Maximum 999
+    $sufF = get-random -Minimum 1000 -Maximum 9999
+
+    $phone = "($area)555-$suff"
+    }
+    $phone
+    }
