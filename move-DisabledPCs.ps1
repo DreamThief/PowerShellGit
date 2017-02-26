c1a01967ea37f6ea582a6dbc76e35fa0e0fd17ac
DreamThief <git@dreamthief.net>
2017-02-26 15:47:13 -0600

    moves disabled computer accounts to desired OU

---
+++ move-DisabledPCs.ps1
@@ -0,0 +1,10 @@
+﻿function move-DisabledPCs {
+
+$disabledPCs = Get-ADComputer -server $server -Credential $creds -filter {enabled -eq $false}
+$ouPath = "OU=TestComputers,DC=dreamthief,DC=co" 
+
+foreach ($pc in $disabledPCs) {
+    move-adobject $pc -TargetPath $ouPath
+
+        }
+    }
