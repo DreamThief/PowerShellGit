fad83278397ed7786850d2c8d5cfe4275e4e307b
DreamThief <git@dreamthief.net>
2017-03-08 20:20:30 -0600

    get-flag ascii art function

---
+++ get-flag.ps1
@@ -0,0 +1,19 @@
+﻿function get-flag {     
+Write-host "▄ ▄ ▄ ▄ ▄ ▄" -BackgroundColor Blue -NoNewline; write-host "                         " -BackgroundColor red
+Write-host " ▀ ▀ ▀ ▀ ▀ " -BackgroundColor Blue -NoNewline; write-host "                         " -BackgroundColor white
+Write-host "▀▄▀▄▀▄▀▄▀▄▀" -BackgroundColor Blue -NoNewline; write-host "                         " -BackgroundColor red
+Write-host "▄ ▄ ▄ ▄ ▄ ▄" -BackgroundColor Blue -NoNewline; write-host "                         " -BackgroundColor white
+Write-host "▄ ▄ ▄ ▄ ▄ ▄" -BackgroundColor Blue -NoNewline; write-host "                         " -BackgroundColor red
+Write-host " ▀ ▀ ▀ ▀ ▀ " -BackgroundColor Blue -NoNewline; write-host "                         " -BackgroundColor white
+Write-host "▀ ▀ ▀ ▀ ▀ ▀" -BackgroundColor Blue -NoNewline; write-host "                         " -BackgroundColor red
+write-host "                                    " -BackgroundColor white
+write-host "                                    " -BackgroundColor red
+write-host "                                    " -BackgroundColor white
+write-host "                                    " -BackgroundColor red
+write-host "                                    " -BackgroundColor white
+write-host "                                    " -BackgroundColor red
+}
+
+    
+#  ■  ▄  ▀
+                             
