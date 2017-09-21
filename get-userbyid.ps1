df752b152a882f2509a61e7ddc55d25e87707d2c
DreamThief <git@dreamthief.net>
2017-09-20 21:05:58 -0500

    initial commit

---
+++ get-userbyid.ps1
@@ -0,0 +1,3 @@
+﻿$date = (get-date).adddays(-5)
+get-aduser -Server $glserver -Credential $creds -filter {whenCreated -gt $date} -Properties * |
+    select name, samaccountname, whencreated,employeenumber
