4220677f29ef9a7f49636ec1f301c7129020e0aa
DreamThief <git@dreamthief.net>
2017-10-24 21:11:44 -0500

    initial comit

---
+++ files-as-switches.ps1
@@ -0,0 +1,13 @@
+﻿$list = $null
+$dir = $null
+$path = $null
+
+$dir = '.\desktop'
+
+$path = get-childitem $dir
+
+ForEach($i in $path){
+    $list +=  '.\documents\' + $i.Name + ' , '
+     }
+
+write-host "somecommand.exe " $list -ForegroundColor green
