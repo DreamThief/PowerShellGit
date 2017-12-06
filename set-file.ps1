31cf342df6f15a7e77686ef8d453f4cccbfa4efe
DreamThief <git@dreamthief.net>
2017-12-05 21:36:26 -0600

    Change or copy files by switch

---
+++ set-file.ps1
@@ -0,0 +1,18 @@
+﻿function set-filez{
+param (
+[switch]$new ,
+[switch]$old
+)
+
+if($new){
+copy-item .\files\newFile.txt .\files\ztest.txt
+}
+elseif($old){
+copy-item .\files\oldFile.txt .\files\ztest.txt
+}
+else{
+write-host "Hey dumbass, you did not do anything!" -ForegroundColor Cyan
+}
+ 
+
+ }
