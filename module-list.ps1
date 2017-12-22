24241a90d9d97492a0f01f50a311368e8f436615
DreamThief <git@dreamthief.net>
2017-12-22 12:54:30 -0600

    Simple add module list

---
+++ module-list.ps1
@@ -0,0 +1,6 @@
+﻿#$moduleList = @('AzureRM.Profile' , 'AzureRM.Resources' , 'Azure.Storage' , 'AzureRM.Storage' , 'AzureRmStorageTable')
+
+$moduleList = @('AzureRM') # -AllowClobber
+
+foreach ($mod in $modulelist)
+            {Find-Module $mod | Install-Module }
