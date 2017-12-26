3ab8b0ab223b4793a0385008bb9d5d027c543a5e
DreamThief <git@dreamthief.net>
2017-12-25 20:01:04 -0600

    update format and add more modules

--- module-list.ps1
+++ module-list.ps1
@@ -1,6 +1,14 @@
 ﻿#$moduleList = @('AzureRM.Profile' , 'AzureRM.Resources' , 'Azure.Storage' , 'AzureRM.Storage' , 'AzureRmStorageTable')
 
-$moduleList = @('AzureRM') # -AllowClobber
+$moduleList = @(
+                'AzureRM', 
+                'posh-git' ,
+                'vmxtoolkit'
+                'EnhancedHTML2' ,
+                'pester' ,
+                'universaldashboard'
+
+                ) 
 
 foreach ($mod in $modulelist)
-            {Find-Module $mod | Install-Module }
+            {Find-Module $mod | Install-Module } # -AllowClobber
