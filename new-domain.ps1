8fe91883a4b3907887d60415ce346c2e6ab86de5
DreamThief <git@dreamthief.net>
2018-06-14 17:46:49 -0500

    Update for typos

--- new-domain.ps1
+++ new-domain.ps1
@@ -1,14 +1,18 @@
 #
 # Windows PowerShell script for AD DS Deployment
 #
+# ! Make sure your hostname is correct on the server.
+# ! Make sure your networking is set up on the server!
+#
+Clear-Host
 
 $domain = @{
     'CreateDnsDelegation'  = $false ;
     'DatabasePath'         = "C:\Windows\NTDS" ;
-    'DomainMode'           = "Win2016" ;   #This can also be "7"
+    'DomainMode'           = "7" ;              #This can also be "Win2016" or soemthing similar
     'DomainName'           = "dreamthief.co" ;
     'DomainNetbiosName'    = "DREAMTHIEF" ;
-    'ForestMode'           = "Win2016" ;  #This can also be "7"
+    'ForestMode'           = "7" ;              # This can also be "Win2016" or something similar
     'InstallDns'           = $true ;
     'LogPath'              = "C:\Windows\NTDS" ;
     'NoRebootOnCompletion' = $false ;
@@ -33,7 +37,7 @@ write-host "Install-ADDSForest" @domain
 # [password]
 
 ##After words
-#Install-WindowsFeature rsat-remoteaccess rsat-adds
+#Install-WindowsFeature rsat-remoteaccess , rsat-adds
 
 #Import-module servermanager ; Get-WindowsFeature | where-object {$_.Installed -eq $True} | format-list DisplayName
 
